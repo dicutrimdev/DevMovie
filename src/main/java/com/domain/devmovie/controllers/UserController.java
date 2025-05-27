c0481f346ff60abb5f42761115c6ef46f39bd105
dicutrimdev <diegocutrimcontato@hotmail.com>
2025-05-27 15:04:39 -0300

    Impl. do Usercontroller e suas respostas http

---
+++ src/main/java/com/domain/devmovie/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.domain.devmovie.controllers;
+
+import lombok.RequiredArgsConstructor;
+import com.domain.devmovie.entities.User;
+import org.springframework.http.HttpStatus;
+import com.domain.devmovie.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        var createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        var user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> getUserByEmail(@RequestParam String email) {
+        var user = userService.getUserByEmail(email);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
