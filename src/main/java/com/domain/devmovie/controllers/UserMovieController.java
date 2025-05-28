cbd06dc3d809714f874010f5fdaa63762104862e
dicutrimdev <diegocutrimcontato@hotmail.com>
2025-05-27 21:43:41 -0300

    Impl. completa do UserMovieController e suas responses http

--- src/main/java/com/domain/devmovie/controllers/UserMovieController.java
+++ src/main/java/com/domain/devmovie/controllers/UserMovieController.java
@@ -1,9 +1,13 @@
 package com.domain.devmovie.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.domain.devmovie.dto.RequestUserMovieDto;
 import com.domain.devmovie.service.UserMovieService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.domain.devmovie.dto.ResponseUserMovieDto;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +16,25 @@ public class UserMovieController {
 
     private final UserMovieService userMovieService;
 
+    @PostMapping("/{userId}/favorites")
+    public ResponseEntity<ResponseUserMovieDto> addFavorite(@PathVariable Long userId,
+                                                            @RequestBody RequestUserMovieDto request) {
+        ResponseUserMovieDto response = userMovieService.addFavorite(userId, request);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/{userId}/favorites")
+    public ResponseEntity<List<ResponseUserMovieDto>> getFavoritesByUser(@PathVariable Long userId) {
+        List<ResponseUserMovieDto> favorites = userMovieService.getFavoritesByUser(userId);
+        return ResponseEntity.ok(favorites);
+    }
+
+
+    @DeleteMapping("/favorites/{favoriteId}")
+    public ResponseEntity<Void> removeFavorite(@PathVariable Long favoriteId) {
+        userMovieService.removeFavorite(favoriteId);
+        return ResponseEntity.noContent().build();
+    }
 
 }
